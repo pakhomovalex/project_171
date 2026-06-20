024d51747e8e9a49c26c58a3e74a9d8be2bb2c47
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2026-06-20 18:57:31 +0200

    fix prodaction

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  transpilePackages: ['zustand'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.example.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'charity-platform-backend-va70.onrender.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-ea16cd9d48134f17bab13b9ace64ae69.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
